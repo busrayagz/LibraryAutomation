f4314c420b52271238d7014986dfd15dd27917a0
Busra <busra.yagiz@hotmail.com>
2022-11-30 22:04:36 -0500

    framework created

--- src/test/resources/queries/US01.sql
+++
@@ -1,8 +0,0 @@
-select id,full_name,email,password,user_group_id,image,
-       extra_data,status,is_admin,start_date,end_date,address from users;
-
-select count(*) from users;
-
-select count(distinct id) from users;
-
-select id from users;
